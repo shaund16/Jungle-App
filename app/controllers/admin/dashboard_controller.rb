2697d20404d3b415a1d3e604f36195e4d1fd60d7
Shaun <s-purslow@hotmail.com>
2021-08-17 20:30:29 +0000

    connect product and categories to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["NAME"], :password =>ENV["PASSWORD"]
   def show
+    @product = Product.count
+    @category = Category.count
   end
 end
